cf0004c49ba360f97b4cd94e16d79f1f7508c8f5
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-25 14:13:57 +0700

    import viewBinding and add control

--- app/src/main/java/com/example/readcontentinternet2505/MainActivity.java
+++ app/src/main/java/com/example/readcontentinternet2505/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.readcontentinternet2505.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addControll();
+    }
+
+    private void addControll() {
+        mainBinding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
     }
 }
